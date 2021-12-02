297e2f53cfd3c77ea37b7eacf60fc8c1cea5c8f2
Codi Kraus <codi.kraus@gmail.com>
2021-12-02 12:31:07 -0600

    Done

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
   // type your code here
+  if (num < 2) {
+    return num;
+  }
+
+  let lastTwo = [0, 1];
+
+  for (let i = 0; i < num - 1; ++i) {
+    const sum = lastTwo[0] + lastTwo[1];
+    lastTwo = [lastTwo[1], sum];
+  }
+
+  return lastTwo[1];
 }
 
 if (require.main === module) {
